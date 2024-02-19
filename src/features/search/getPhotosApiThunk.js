00990abd70afed6991a6bbe7e28b6b117cc61a8d
Ricardo Halmoguera <halmoguerar@gmail.com>
2024-02-19 21:19:07 +0100

    FIX. ENV VAR

--- src/features/search/getPhotosApiThunk.js
+++ src/features/search/getPhotosApiThunk.js
@@ -1,6 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit"
 
-
+const URL_API = import.meta.env.URL_API
+const CLIENT_ID = import.meta.env.CLIENT_ID
 
 export const getRandomPhotosThunk = createAsyncThunk(
   "photo/getRandomPhotos",
